6a6582e8ebc209427ae398ee27184c8570f6b7ef
Jan Fellner <jan@estos.de>
2023-03-23 17:33:40 +0100

    BUILDSYS-305 fixed stacktrace for Windows

--- cpp-lib/src/SNACCDeprecated.cpp
+++ cpp-lib/src/SNACCDeprecated.cpp
@@ -19,7 +19,7 @@ void SNACCDeprecated::DeprecatedObject(const char* szModuleName, const char* szO
     if (!m_pCallback)
         return;
 
-    std::list<std::string> callStack = GetStackTrace();
+    std::list<std::string> callStack = GetStackTrace(2);
 
     m_pCallback->DeprecatedObject(szModuleName, szObjectName, callStack);
 }
@@ -28,7 +28,7 @@ void SNACCDeprecated::DeprecatedMethod(const char* szModuleName, const char* szM
     if (!m_pCallback)
         return;
 
-    std::list<std::string> callStack = GetStackTrace();
+    std::list<std::string> callStack = GetStackTrace(2);
 
     m_pCallback->DeprecatedMethod(szModuleName, szMethodName, direction, callStack);
 }
@@ -36,14 +36,26 @@ void SNACCDeprecated::DeprecatedMethod(const char* szModuleName, const char* szM
 std::list<std::string> SNACCDeprecated::GetStackTrace(int remove /*= 1*/) {
     std::list<std::string> stackTrace;
 #ifdef _WIN32
+    SymInitialize(GetCurrentProcess(), NULL, TRUE);
     void* trace[256];
-    int size = CaptureStackBackTrace(0, 256, trace, NULL);
+    int size = CaptureStackBackTrace(remove, 256, trace, NULL);
     for (int i = 0; i < size; i++) {
-        const char* symbol = new char[MAX_PATH];
-        SymFromAddr(GetCurrentProcess(), (DWORD64)(trace[i]), NULL, (SYMBOL_INFO*)&symbol);
-        stackTrace.push_back(symbol);
-        delete [] symbol;
+        const int kMaxNameLength = 256;
+        DWORD_PTR frame = reinterpret_cast<DWORD_PTR>(trace[i]);
+        const int iSize = (sizeof(SYMBOL_INFO) + kMaxNameLength * sizeof(wchar_t) + sizeof(ULONG64) - 1) / sizeof(ULONG64);
+        ULONG64 buffer[iSize];
+        PSYMBOL_INFO symbol = reinterpret_cast<PSYMBOL_INFO>(&buffer[0]);
+        memset(symbol, 0x00, iSize);
+        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+        symbol->MaxNameLen = kMaxNameLength - 1;
+        DWORD64 sym_displacement = 0;
+        if (SymFromAddr(GetCurrentProcess(), frame, &sym_displacement, symbol)) {
+            if (strcmp(symbol->Name, "wmain") == 0)
+                break;
+            stackTrace.push_back(symbol->Name);
+        }
     }
+    SymCleanup(GetCurrentProcess());
 #elif defined(_WIN32) || defined(WIN32)
     void* trace[256];
     int size = backtrace(trace, 256);
