79aeed88a5f6bd7abdd43a85ee34e883670efca4
Jan Fellner <jan@estos.de>
2023-06-01 16:20:50 +0200

    BUILDSYS-308 bumped version to 5.0.5

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION		"5.0.4"
-#define VERSION_RC	5,0,4
-#define RELDATE		"31.05.2023"
+#define VERSION		"5.0.5"
+#define VERSION_RC	5,0,5
+#define RELDATE		"01.06.2023"
 
 #endif // VERSION_H
