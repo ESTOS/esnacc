c2b41271b4f091c5647a6b99a5d6d4bef19b00c4
Jan Fellner <jan@estos.de>
2023-06-02 14:37:37 +0200

    BUILDSYS-311 bumped version to 5.0.6

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION		"5.0.5"
-#define VERSION_RC	5,0,5
-#define RELDATE		"01.06.2023"
+#define VERSION		"5.0.6"
+#define VERSION_RC	5,0,6
+#define RELDATE		"02.06.2023"
 
 #endif // VERSION_H
