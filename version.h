cd4bbeb71e275ed586b7b8b74df91e986f469f26
Jan Fellner <jan@estos.de>
2023-06-16 16:48:16 +0200

    BUILDSYS-322 Bumped version and generated the matching files for them

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.0.9"
-#define VERSION_RC 5, 0, 9
-#define RELDATE "14.06.2023"
+#define VERSION "5.0.10"
+#define VERSION_RC 5, 0, 10
+#define RELDATE "16.06.2023"
 
 #endif // VERSION_H
