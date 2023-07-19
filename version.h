c2adb2c3a9bd601c9709b18f9d59634773abee6d
Jan Fellner <jan@estos.de>
2023-07-19 13:25:06 +0200

    BUILDSYS-342 bumped version to 5.0.15

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.0.14"
-#define VERSION_RC 5, 0, 14
-#define RELDATE "18.07.2023"
+#define VERSION "5.0.15"
+#define VERSION_RC 5, 0, 15
+#define RELDATE "19.07.2023"
 
 #endif // VERSION_H
