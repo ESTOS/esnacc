bdcd9ec39249b9be983149e3daa4dfad01d48f41
Jan Fellner <jan@estos.de>
2023-08-16 11:55:02 +0200

    PROCALL-4056 Bumped new version

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.0.16"
-#define VERSION_RC 5, 0, 16
-#define RELDATE "10.08.2023"
+#define VERSION "5.0.17"
+#define VERSION_RC 5, 0, 17
+#define RELDATE "16.08.2023"
 
 #endif // VERSION_H
