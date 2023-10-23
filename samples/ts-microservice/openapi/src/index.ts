ccb7c817b76f30340b17d60898bdd2b1bea7d17e
Dominik Kerzel <50227667+kerzeld@users.noreply.github.com>
2023-10-23 13:00:59 +0200

    update sdk version

--- samples/ts-microservice/openapi/src/index.ts
+++ samples/ts-microservice/openapi/src/index.ts
@@ -1,30 +1,32 @@
 import "@estos/esnacc-openapi-sdk/dist/esnacc-openapi-sdk.css";
 import SDK from "@estos/esnacc-openapi-sdk";
 
-console.log(SDK)
-
 SDK({
     domId: "ui", schemas: [
         { schemaUrl: "/schema/ENetUC_Common.json", label: "ENetUC_Common" },
         {
-            schemaUrl: "/schema/ENetUC_Settings_Manager.json", label: "ENetUC_Settings_Manager", "servers": [
-                {
-                    "url": "ws://localhost:3020/ws"
-                },
-                {
-                    "url": "http://localhost:3020/rest"
-                }
-            ],
+            schemaUrl: "/schema/ENetUC_Settings_Manager.json", label: "ENetUC_Settings_Manager", "injectSpec": {
+                servers: [
+                    {
+                        "url": "ws://localhost:3020/ws"
+                    },
+                    {
+                        "url": "http://localhost:3020/rest"
+                    }
+                ]
+            },
         },
         {
-            schemaUrl: "/schema/ENetUC_Event_Manager.json", label: "ENetUC_Event_Manager", "servers": [
-                {
-                    "url": "ws://localhost:3020/ws"
-                },
-                {
-                    "url": "http://localhost:3020/rest"
-                }
-            ],
+            schemaUrl: "/schema/ENetUC_Event_Manager.json", label: "ENetUC_Event_Manager", "injectSpec": {
+                servers: [
+                    {
+                        "url": "ws://localhost:3020/ws"
+                    },
+                    {
+                        "url": "http://localhost:3020/rest"
+                    }
+                ]
+            },
         },
     ]
 })
