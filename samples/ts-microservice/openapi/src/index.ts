467794f0e0132ac3524b971ff6da140a0cccbf41
Dominik Kerzel <50227667+kerzeld@users.noreply.github.com>
2023-10-23 14:46:19 +0200

    fixed openapi sample

--- samples/ts-microservice/openapi/src/index.ts
+++ samples/ts-microservice/openapi/src/index.ts
@@ -3,9 +3,9 @@ import SDK from "@estos/esnacc-openapi-sdk";
 
 SDK({
     domId: "ui", schemas: [
-        { schemaUrl: "/schema/ENetUC_Common.json", label: "ENetUC_Common" },
+        { schemaUrl: "/ENetUC_Common.json", label: "ENetUC_Common" },
         {
-            schemaUrl: "/schema/ENetUC_Settings_Manager.json", label: "ENetUC_Settings_Manager", "injectSpec": {
+            schemaUrl: "/ENetUC_Settings_Manager.json", label: "ENetUC_Settings_Manager", "injectSpec": {
                 servers: [
                     {
                         "url": "ws://localhost:3020/ws"
@@ -17,7 +17,7 @@ SDK({
             },
         },
         {
-            schemaUrl: "/schema/ENetUC_Event_Manager.json", label: "ENetUC_Event_Manager", "injectSpec": {
+            schemaUrl: "/ENetUC_Event_Manager.json", label: "ENetUC_Event_Manager", "injectSpec": {
                 servers: [
                     {
                         "url": "ws://localhost:3020/ws"
