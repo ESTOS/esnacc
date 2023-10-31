c64e77cf41f938de7fddd6e035ab0c6e8be962d5
Jan Fellner <jan@estos.de>
2023-10-31 09:01:20 +0100

    UCAAS-132 bumped new Version 5.0.24

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.0.23"
-#define VERSION_RC 5, 0, 23
-#define RELDATE "20.10.2023"
+#define VERSION "5.0.24"
+#define VERSION_RC 5, 0, 24
+#define RELDATE "31.10.2023"
 
 #endif // VERSION_H
