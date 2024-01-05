80ae674e23744967d86e2182626723a6cc38588e
Jan Fellner <jan@estos.de>
2024-01-05 14:48:13 +0100

    BUILDSYS-389 bumped to new version 5.1.0

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.0.27"
-#define VERSION_RC 5, 0, 27
-#define RELDATE "12.12.2023"
+#define VERSION "5.1.0"
+#define VERSION_RC 5, 1, 0
+#define RELDATE "05.01.2024"
 
 #endif // VERSION_H
