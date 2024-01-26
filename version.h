cc65a7a78556596695b3b47fb04d69450582ce0c
Jan Fellner <jan@estos.de>
2024-01-26 12:34:48 +0100

    UCAAS-229 bumped new version 5.1.1

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.1.0"
-#define VERSION_RC 5, 1, 0
-#define RELDATE "05.01.2024"
+#define VERSION "5.1.1"
+#define VERSION_RC 5, 1, 1
+#define RELDATE "26.01.2024"
 
 #endif // VERSION_H
