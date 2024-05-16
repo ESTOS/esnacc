58fab0b5d97cd731f13d409834ddf50eb03c4f4f
Jan Fellner <jan@estos.de>
2024-05-16 16:41:52 +0200

    BUILDSYS-380 bumped esnacc to 6.0.8

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "6.0.7"
-#define VERSION_RC 6, 0, 7
-#define RELDATE "15.05.2024"
+#define VERSION "6.0.8"
+#define VERSION_RC 6, 0, 8
+#define RELDATE "16.05.2024"
 
 #endif // VERSION_H
