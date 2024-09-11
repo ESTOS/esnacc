c19bc59462362593a7952def4fdcb4b5e507e096
Jan Fellner <jan@estos.de>
2024-09-11 13:01:55 +0200

    BUILDSYS-438 Fixed compiler warnings and errors

---
+++ c-lib/include/platform-functions.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	char* mytolower(char* s);
+	int myfopen(FILE** pFile, const char* filename, const char* mode);
+
+#ifdef __cplusplus
+}
+#endif
