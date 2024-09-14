49b5c3b7a5b5d0d340cec9ca7967933954b98dfc
Jan Fellner <jan@estos.de>
2024-09-14 21:33:51 +0200

    BUILDSYS-443 updated version 6.0.15

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "6.0.14"
-#define VERSION_RC 6, 0, 14
-#define RELDATE "13.09.2024"
+#define VERSION "6.0.15"
+#define VERSION_RC 6, 0, 15
+#define RELDATE "14.09.2024"
 
 #endif // VERSION_H
