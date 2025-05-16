5f11450be201484dfe4638a3d3bbbbe36e0ee9c0
Jan Fellner <jan@estos.de>
2025-05-16 17:49:48 +0200

    UCAAS-880 bumped version to 5.1.4

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "5.1.3"
-#define VERSION_RC 5, 1, 3
-#define RELDATE "10.04.2024"
+#define VERSION "5.1.4"
+#define VERSION_RC 5, 1, 4
+#define RELDATE "16.05.2025"
 
 #endif // VERSION_H
