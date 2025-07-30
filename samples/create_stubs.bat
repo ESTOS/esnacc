5a7eb27d9198e6f710a67acda788976efb8992ee
Jan Fellner <jan@estos.de>
2025-07-30 10:17:23 +0200

    UCAAS-974 Fix missing asn1interfaceversion files

--- samples/create_stubs.bat
+++ samples/create_stubs.bat
@@ -23,7 +23,7 @@ set FILES=%CD%\interface\*.asn1
 rem Building browser client stubs...
 echo Building browser client stubs...
 PUSHD ts-microservice\client\src\stub
-SET COMMAND=-JT -j -RTS_CLIENT_BROWSER -node:%NODEVERSION% -comments %FILES%
+SET COMMAND=-JT -j -RTS_CLIENT_BROWSER -node:%NODEVERSION% -comments -versionfile %FILES%
 echo %COMPILER% %COMMAND%
 %COMPILER% %COMMAND%
 echo.
@@ -33,7 +33,7 @@ POPD
 rem Building node server stubs...
 echo Building node server stubs...
 PUSHD ts-microservice\server\src\stub
-SET COMMAND=-JTE -j -RTS_SERVER -node:%NODEVERSION% -comments %FILES%
+SET COMMAND=-JTE -j -RTS_SERVER -node:%NODEVERSION% -comments -versionfile %FILES%
 echo %COMPILER% %COMMAND%
 %COMPILER% %COMMAND%
 echo.
@@ -43,14 +43,14 @@ POPD
 rem Building openapi files...
 echo Building openapi files...
 PUSHD ts-microservice\openapi\example
-SET COMMAND=-JO -node:%NODEVERSION% -comments %FILES%
+SET COMMAND=-JO -node:%NODEVERSION% -comments -versionfile %FILES%
 echo %COMPILER% %COMMAND%
 %COMPILER% %COMMAND%
 echo.
 if NOT %ERRORLEVEL% == 0 echo Build has failed, check console...>&2
 POPD
 PUSHD ts-microservice\openapi\unpkg-example
-SET COMMAND=-JO -node:%NODEVERSION% -comments %FILES%
+SET COMMAND=-JO -node:%NODEVERSION% -comments -versionfile %FILES%
 echo %COMPILER% %COMMAND%
 %COMPILER% %COMMAND%
 echo.
