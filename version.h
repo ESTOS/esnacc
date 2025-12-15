eb2cb3f1ac7a5e93efbee0223d81838c869a5f97
Jan Fellner <jan@estos.de>
2025-12-15 15:03:52 +0100

    BUILDSYS-568 bumped version to 6.0.35

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "6.0.34"
-#define VERSION_RC 6, 0, 34
-#define RELDATE "02.12.2025"
+#define VERSION "6.0.35"
+#define VERSION_RC 6, 0, 35
+#define RELDATE "15.12.2025"
 
 #endif // VERSION_H
