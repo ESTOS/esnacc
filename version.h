37cc720c78df9d9c719995734bff1d1065a4bd74
Jan Fellner <jan@estos.de>
2026-03-26 16:18:49 +0100

    BUILDSYS-578 Bumped to 7.0.2

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "7.0.1"
-#define VERSION_RC 7, 0, 1
+#define VERSION "7.0.2"
+#define VERSION_RC 7, 0, 2
 #define RELDATE "26.03.2026"
 
 #endif // VERSION_H
